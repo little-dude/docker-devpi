8044685f90bcbf37c39405e49eeaae3c05a35cca
corentih <corentin.henry@alcatel-lucent.com>
2015-04-09 09:57:23 -0700

    Use Nginx as reverse proxy
    Use Nginx to forward devpi-client requests to devpi-server.
    
    Changes introduced :
    --------------------
    
    Components :
        - Nginx is installed
    
    Behavior :
        - Nginx listens on 0.0.0.0:3141. Ip and port are configurable via
          NGINX_IP and NGINX_PORT environment variables.
        - if NGINX_ENABLETLS is true, nginx also listens on port 3142 for
          HTTPS requests. TLS is configurable via multiple environment
          variables that are documented in the example environment file.
        - devpi-server listens on 127.0.0.1:3143. Ip and port are
          configurable via DEVPI_SERVERIP and DEVPI_SERVERPORT environment
          variables.
    
    Files :
        - devpi-nginx.env : example of environment file to use with this
          container. Environment are documented there.
        - nginx-tpl.conf : nginx configuration template
        - devpi.crt and devpi.key : dumb certificate added to the container
          and used by default when https is enabled. Users are encouraged
          not to use it. The README explains how to use custom certs.
    
    References :
    ------------
    
        - https://github.com/nginxinc/docker-nginx the official nginx docker
          image repo
        - https://dantehranian.wordpress.com/2014/09/03/a-local-caching-proxy-for-pypi-python-org-via-docker/
          blog post about nginx+docker
        - doc.devpi.net/latest/userman/index.html devpi documentation

--- README.md
+++ README.md
@@ -26,3 +26,35 @@ This repository contains **Dockerfile** of [Devpi](http://doc.devpi.net/) for [D
 
 Devpi creates a user named `root` by default, its password can be set with
 `DEVPI_PASSWORD` environment variable.
+
+For more options, you can use an environment file, like `devpi-nginx.env`.
+
+#### Use HTTPS
+
+If `NGINX_ENABLETLS` environment variable is set to true, nginx will be
+configured to use TLS.  By default it listens on `0.0.0.0:3141` but this is
+configurable via `NGINX_IP` and `NGINX_TLSPORT`. 
+
+This image comes with a self signed certificate, but it is recommended that you
+use your own certificate. To do so, you can either re-build the container with
+your own certificates (see the Dockerfile), or mount the ceertificate and its
+key when you start the container. Nginx expects users certificate and
+its key to be located in `/opt/nginx/certs/custom/devpi.crt` and
+`/opt/nginx/certs/custom/devpi.key`. So after generating `devpi.crt` and `devpi.key`,
+one can start the container with :
+
+```
+$ cat env_file.txt
+# nginx will listen on every interface
+NGINX_IP=0.0.0.0
+# port for http
+NGINX_PORT=3141
+# https configuration
+NGINX_ENABLETLS=true
+NGINX_TLSPORT=3142
+NGINX_TLSPATH=/opt/nginx/certs/custom
+NGINX_TLSCERT=cert.crt
+NGINX_TLSKEY=certKey.key
+
+$ docker run  -d --name devpi -p 3141:3141 -p 3142:3142 --env-file=env_file.txt -v /path/to/certs:/opt/nginx/certs/custom scrapinghub/devpi
+```
--- run.sh
+++ run.sh
@@ -1,15 +1,57 @@
 #!/bin/bash
 set -e
 set -x
-export DEVPI_SERVERDIR=/mnt
-export DEVPI_CLIENTDIR=/tmp/devpi-client
-[[ -f $DEVPI_SERVERDIR/.serverversion ]] || initialize=yes
 
-devpi-server --start --host 0.0.0.0 --port 3141
-if [[ $initialize = yes ]]; then
-  devpi use http://localhost:3141
-  devpi login root --password=''
-  devpi user -m root password="${DEVPI_PASSWORD}"
-  devpi index -y -c public pypi_whitelist='*'
-fi
-tail -f $DEVPI_SERVERDIR/.xproc/devpi-server/xprocess.log
+# Default environment if none is provided
+DEVPI_SERVERIP=${DEVPI_SERVERIP:-127.0.0.1}
+DEVPI_SERVERPORT=${DEVPI_SERVERPORT:-3143}
+DEVPI_SERVERDIR=${DEVPI_SERVERDIR:-/mnt}
+DEVPI_CLIENTDIR=${DEVPI_CLIENTDIR:-/tmp/devpi-client}
+DEVPI_CLIENTPASSWORD=${DEVPI_CLIENTPASSWORD:-""}
+DEVPI_LDAPCONFILE=${DEVPI_LDAPCONFILE:-""}
+
+NGINX_IP=${NGINX_IP:-0.0.0.0}
+NGINX_PORT=${NGINX_PORT:-3141}
+NGINX_ENABLETLS=${NGINX_ENABLETLS:-false}
+NGINX_TLSPORT=${NGINX_TLSPORT:-3142}
+NGINX_TLSPATH=${NGINX_TLSPATH:-/opt/nginx/certs/default}
+NGINX_TLSCERT=${NGINX_TLSCERT:-devpi.crt}
+NGINX_TLSKEY=${NGINX_KEY:-devpi.key}
+
+function start_devpi_server () {
+    # Start the server
+    devpi-server --start --host ${DEVPI_SERVERIP} --port ${DEVPI_SERVERPORT} &> stdout
+    
+    # Configure the default client if necessary
+    if [[ ! -f ${DEVPI_SERVERDIR}/.serverversion ]]; then
+      devpi use http://${DEVPI_SERVERIP}:${DEVPI_SERVERPORT}
+      devpi login root --password="${DEVPI_CLIENTPASSWORD}"
+      devpi user -m root password="${DEVPI_CLIENTPASSWORD}"
+      devpi index -y -c public pypi_whitelist='*'
+    fi
+}
+
+function start_nginx () {
+    nginx_template_file=/opt/nginx/nginx-tpl.conf
+
+    # Uncomment lines that enable TLS in template file
+    if [ ${NGINX_ENABLETLS} = true ]; then
+        sed -i "s/#{uncomment_if_tls_enabled}\s*//g" ${nginx_template_file}
+    fi
+
+    # Escape all \ & and / for every variable of the current env, and replace all
+    # ${var} occurences in nginx conf template file with the corresponding variable
+    # value
+    env | sed 's/[\%]/\\&/g;s/\([^=]*\)=\(.*\)/s%${\1}%\2%/' | sed -f- ${nginx_template_file} | tee /tmp/nginx.conf    
+
+    
+    # forward nginx logs to stdout and stderr 
+    ln -sf /dev/stdout /var/log/nginx/access.log
+    ln -sf /dev/stderr /var/log/nginx/error.log
+
+    # Start nginx
+    nginx -c /tmp/nginx.conf > /dev/null
+}
+
+start_devpi_server
+start_nginx
